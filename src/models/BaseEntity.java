84e80a0f4e14b7fb8480a83e4387e0a6bcea3a9f
Tim <nsrv.tim@gmail.com>
2022-11-18 09:45:02 +0600

    first commit

---
+++ src/models/BaseEntity.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Date;
+
+public abstract class BaseEntity {
+    private Long id;
+    private String name;
+    private boolean active;
+    private Date addDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+}
